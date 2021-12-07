392150966b33f8f2a41623fb26424fe795db149f
kreiniger <klausarreiniger@gmail.com>
2021-12-07 14:53:54 -0300

    installing material ui libraries

--- src/bullet_components/AppNavBar.js
+++ src/bullet_components/AppNavBar.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const AppNavBar = ({title}) => (
-    <div>
-        <h1>{title}</h1>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
 )
 
 export default AppNavBar

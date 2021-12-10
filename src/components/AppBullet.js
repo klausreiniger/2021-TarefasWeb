23783b3e4458069d8c2cbda6895695f747096c44
kreiniger <klausarreiniger@gmail.com>
2021-12-10 16:19:55 -0300

    a bit of styling

--- src/components/AppBullet.js
+++ src/components/AppBullet.js
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
-import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 
-const AppBulletHeader = styled.h1`
+const styledIcon = styled(CheckIcon)`
+    min-width: 5px;
+    min-height: 5px;
     color: blue;
+    opacity: ${props => props.isSet == "true" ? "1" : "0.2"}
 `;
 
-class AppBullet extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return (
-            <li key={this.props.key}>
-                <div>
-                    <AppBulletHeader>{this.props.type}</AppBulletHeader>
-                    <Typography paragraph>{this.props.content}</Typography>
-                </div>
-            </li>
-        )
-    }
+function AppBullet(props) {
+    const [status, setstatus] = useState(false)
+    return (
+        <Card key={props.key} sx={{ minWidth: 275 }}>
+        <CardContent>
+           <Typography variant="h5" component="div">
+             {props.type}
+           </Typography>
+           <Typography variant="body2">
+             {props.content}
+           </Typography>
+         </CardContent>
+         <CardActions>
+           <Button size="small" onclick={
+               () => {setstatus(true)}
+           }>
+                <CheckIcon/>               
+                <styledIcon isSet={status ? "true" : "false"}/>
+            </Button>
+            <Button size="small" onclick={
+               () => {setstatus(false)}
+           }>
+                <styledIcon as="ClearIcon" isSet={status ? "false" : "true"}/>
+            </Button>
+         </CardActions>
+       </Card>
+    )
 }
 
 export default AppBullet
